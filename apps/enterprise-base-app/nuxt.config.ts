5565f2e3b39a3a33a73a1f10bbd3f8ad3cba6b8d
Alfred Narjes <alfred.narjes@gmail.com>
2025-05-27 16:05:58 +0200

    supabase integration and setup complete and tested, same state as main on 57b1b77

--- apps/enterprise-base-app/nuxt.config.ts
+++ apps/enterprise-base-app/nuxt.config.ts
@@ -6,7 +6,12 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
+    '@nuxtjs/supabase',
   ],
+  supabase: {
+    //? Deactivates the automatic login redirect
+    redirect: false
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true }
 })
